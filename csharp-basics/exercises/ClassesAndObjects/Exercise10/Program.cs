20f58de3d33bb4da5f10152efd60a233e744129b
VnkNulle <taigeahetsu@gmail.com>
2023-08-02 20:36:21 +0300

    Exercise1

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise10/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace VideoStore
+{
+
+    class Program
+    {
+        public static VideoStore _store;
+        public static void Main(string[] args)       
+        {
+            _store = new VideoStore(); 
+            while (true)
+            {
+                Console.WriteLine("Choose the operation you want to perform ");
+                Console.WriteLine("Choose 0 for EXIT");
+                Console.WriteLine("Choose 1 to fill video store");
+                Console.WriteLine("Choose 2 to rent video (as user)");
+                Console.WriteLine("Choose 3 to return video (as user)");
+                Console.WriteLine("Choose 4 to list inventory");
+
+                int n = Convert.ToByte(Console.ReadLine());            
+
+                switch (n)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        FillVideoStore();
+                        break;
+                    case 2:
+                        RentVideo();
+                        break;
+                    case 3:
+                        ReturnVideo();
+                        break;
+                    case 4:
+                        ListInventory();
+                        break;
+                    case 5:
+                        RecieveRating();
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private static void RecieveRating()
+        {
+            var title = "spider man";
+            _store.ReturnedVideo(title);
+        }
+
+        private static void ListInventory()
+        {
+
+        }
+
+        private static void FillVideoStore()
+        {
+            
+        }
+
+        private static void RentVideo()
+        {
+            var title = "spider man";
+            _store.CheckOut(title);
+        }
+
+        private static void ReturnVideo()
+        {
+            var title = "spider man";
+            _store.ReturnedVideo(title);
+        }
+    }
+}
---
+++ csharp-basics/exercises/FlowOfControl/Exercise1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the 1st number: ");
+            var input1 = Console.ReadLine();
+
+            Console.WriteLine("Input the 2nd number: ");
+            var input2 = Console.ReadLine();
+
+            Console.WriteLine("Input the 3rd number: ");
+            var input3 = Console.ReadLine();
+
+            int number1 = int.Parse(input1);
+            int number2 = int.Parse(input2);
+            int number3 = int.Parse(input3);
+
+
+            int largestNumber = number1;
+
+            if (number2 > largestNumber)
+            {
+                largestNumber = number2;
+            }
+
+            if (number3 > largestNumber)
+            {
+                largestNumber = number3;
+            }
+
+            // Print the result
+            Console.WriteLine("The largest number is: " + largestNumber);
+        }
+    }
+}
+
+
+
+
+
